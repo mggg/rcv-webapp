ea629fd1dc62ec3ed7031f0759b700753fede3a5
Dylan Phelan <dtphelan1@gmail.com>
2020-10-17 16:29:59 -0400

    Basic package additions

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/AppHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+function AppHeader() {
+  return (
+    <Navbar bg="light">
+      <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+    </Navbar>
+  )
+}
+
+export default AppHeader;
