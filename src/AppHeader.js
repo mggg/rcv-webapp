0cbd67049fb128bdc39c4f9a52610592ef09aeed
Dylan Phelan <dtphelan1@gmail.com>
2020-11-03 18:24:29 -0500

    lint fixes

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 
 function AppHeader(props) {
   return (
