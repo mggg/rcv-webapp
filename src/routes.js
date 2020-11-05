2fda81f549ca7d867139e96886e86df8373eeb84
Dylan Phelan <dtphelan1@gmail.com>
2020-11-05 16:01:53 -0500

    Reorder routes for about page to work

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,14 @@ import AboutPage from "./AboutPage";
 // Config file serving as a single point of truth for all application routes
 const routes = [
   {
-    path: "/",
-    display: "Home",
-    component: SimulationPage,
-  },
-  {
     path: "/about",
     display: "About",
     component: AboutPage,
+  },
+  {
+    path: "/",
+    display: "Home",
+    component: SimulationPage,
   },
 ];
 
