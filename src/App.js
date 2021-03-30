6b5df59862eb431f236aa69a3e8f94c8f1b5f27a
Dylan Phelan <dtphelan1@gmail.com>
2021-03-30 09:27:06 -0400

    Hash router instead of browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import AppHeader from "./AppHeader";
 import { routes } from "./pages/routes";
 
